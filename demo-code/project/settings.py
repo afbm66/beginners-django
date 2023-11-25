77941162249f64204541efcc2f2bc10624f0ea53
a.fernandezbravo@ufv.es <a.fernandezbravo@ufv.es>
2023-11-25 18:25:00 +0100

    Whitenoise line added to INSTALLED_APPS

--- demo-code/project/settings.py
+++ demo-code/project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap4',
     'relecloud.apps.RelecloudConfig',
+    'whitenoise.runserver_nostatic',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
